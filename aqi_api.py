2c7cca5efefc52db9f4f034f63b06f0237561fee
Gladiator07 <atharvaaingle@gmail.com>
2022-06-08 17:34:46 +0530

    rollback

--- aqi_api.py
+++ aqi_api.py
@@ -9,7 +9,6 @@ from utils.aqi_forecast import fetch_future_air_data
 from utils.utils_data import air_content_mean
 
 app = FastAPI()
-loaded_model = pickle.load(open("models/aqi_rf.pkl", 'rb'))
 
 class AQIDtypes(BaseModel):
     city: str
@@ -23,6 +22,7 @@ async def predict_aqi(city: AQIDtypes = None):
     data_today, data_first, data_second, data_third, data_fourth, data_fifth, pre_data_first,pre_data_second,pre_data_third, pre_data_fourth, pre_data_fifth = fetch_future_air_data(city)
     data_dict = air_content_mean(data_today)
     air_contents = list(data_dict.values())
+    loaded_model = pickle.load(open("models/aqi_rf.pkl", 'rb'))
     predicted_aqi = loaded_model.predict(np.array([air_contents]))[0]
     return {
         "AQI": predicted_aqi,

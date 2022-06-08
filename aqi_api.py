54e2d63127a466d9daa253a2001c747b917c64e9
Gladiator07 <atharvaaingle@gmail.com>
2022-06-08 17:22:51 +0530

    model init at first

--- aqi_api.py
+++ aqi_api.py
@@ -19,8 +19,6 @@ def home():
 
 @app.post("/predict")
 async def predict_aqi(city: AQIDtypes = None):
-
-    loaded_model = pickle.load(open("models/aqi_rf.pkl", 'rb'))
     data_today, data_first, data_second, data_third, data_fourth, data_fifth, pre_data_first,pre_data_second,pre_data_third, pre_data_fourth, pre_data_fifth = fetch_future_air_data(city)
     data_dict = air_content_mean(data_today)
     air_contents = list(data_dict.values())
@@ -31,5 +29,6 @@ async def predict_aqi(city: AQIDtypes = None):
     }
 
 if __name__ == '__main__':
+    loaded_model = pickle.load(open("models/aqi_rf.pkl", 'rb'))
 
     uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)

025dcee7c1946e1f9ff265de145cef8f3123992a
Gladiator07 <atharvaaingle@gmail.com>
2022-06-08 16:22:08 +0530

    upd

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker aqi_api:app
+web: gunicorn -w 3 -k uvicorn.workers.UvicornWorker aqi_api:app
--- aqi_api.py
+++ aqi_api.py
@@ -14,13 +14,14 @@ class AQIDtypes(BaseModel):
 
 
 @app.post('/predict')
-async def predict_api(city: AQIDtypes):
+async def predict_aqi(city: AQIDtypes):
 
     loaded_model = pickle.load(open("models/aqi_rf.pkl", 'rb'))
     data_today, data_first, data_second, data_third, data_fourth, data_fifth, pre_data_first,pre_data_second,pre_data_third, pre_data_fourth, pre_data_fifth = fetch_future_air_data(city)
     data_dict = air_content_mean(data_today)
     air_contents = list(data_dict.values())
     predicted_aqi = loaded_model.predict(np.array([air_contents]))[0]
+    
     return {
         "AQI": predicted_aqi,
         **data_dict
